9dc664f2ed9d7ef657744b9bd0ce250811a52de5
Jesse Nusbaumer <nusbaume@ucar.edu>
2020-03-20 17:15:40 -0600

    Modified CamDiag init to read in namelist file variables, and cleaned up run_diag and example_namelist.yaml.

--- lib/CamDiag.py
+++ lib/CamDiag.py
@@ -12,7 +12,7 @@ plotting methods themselves.
 #++++++++++++++++++++++++++++++
 
 import sys
-import os
+import os.path
 
 #Check if "PyYAML" is present in python path:
 try:
@@ -44,10 +44,42 @@ for root, dirs, files in os.walk(_DIAG_SCRIPTS_PATH):
     for dirname in dirs:
         sys.path.append(os.path.join(root,dirname))
 
+#################
+#Helper functions
+#################
 
-##################################
-# CAM diagnostics class (cam_diag)
-##################################
+#++++++++++++++++++++++++++++++++
+#Script message and exit function
+#++++++++++++++++++++++++++++++++
+
+def end_diag_script(msg):
+
+    """
+    Prints message, and then exits script.
+    """
+
+    print("\n")
+    print(msg)
+    sys.exit(1)
+
+def read_namelist_obj(namelist_obj, varname):
+
+    """
+    Checks if variable/list/dictionary exists in
+    namelist object,and if so returns it.
+    """
+
+    try:
+        var = namelist_obj[varname]
+    except:
+       raise KeyError("'{}' not found in namelist file.  Please see 'example_namelist.yaml'.".format(varname))
+
+    #return variable/list/dictionary:
+    return var
+
+######################################
+#Main CAM diagnostics class (cam_diag)
+######################################
 
 class CamDiag:
 
@@ -75,7 +107,44 @@ class CamDiag:
         Initalize CAM diagnostics object.
         """
 
-        print(namelist_file)
+        #Check that YAML file actually exists:
+        if not os.path.exists(namelist_file):
+            raise FileNotFoundError("'{}' file not found.".format(namelist_file))
+
+        #Open YAML file:
+        with open(namelist_file) as nfil:
+            #Load YAML file:
+            namelist = yaml.load(nfil, Loader=yaml.SafeLoader)
+
+        #Add basic diagnostic info to object:
+        self.__basic_info = read_namelist_obj(namelist, 'diag_basic_info')
+
+        #Check if CAM climatology files will be calculated:
+        try:
+            if namelist['diag_cam_climo']['calc_cam_climo']:
+                self.__cam_climo_info = read_namelist_obj(namelist, 'diag_cam_climo')
+        except:
+            raise KeyError("'calc_cam_climo' in 'diag_cam_climo' not found in namelist file.  Please see 'example_namelist.yaml'.")
+
+        #Check if CAM baseline climatology files will be calculated:
+        if not self.__basic_info['compare_obs']:
+            try:
+                if namelist['diag_cam_baseline_climo']['calc_bl_cam_climo']:
+                    self.__cam_bl_climo_info = read_namelist_obj(namelist, 'diag_cam_baseline_climo')
+            except:
+                raise KeyError("'calc_bl_cam_climo' in 'diag_cam_baseline_climo' not found in namelist file.  Please see 'example_namelist.yaml'.")
+
+        #Add averaging script name:
+        self.__averaging_script = read_namelist_obj(namelist, 'averaging_script')
+
+        #Add regridding script name:
+        self.__regridding_script = read_namelist_obj(namelist, 'regridding_script')
+
+        #Add plotting script names:
+        self.__plot_scripts = read_namelist_obj(namelist, 'plotting_scripts')
+
+        #Add CAM variable list:
+        self.__diag_var_list = read_namelist_obj(namelist, 'diag_var_list')
 
     def create_plots(self):
 
@@ -85,5 +154,12 @@ class CamDiag:
 
         #Notify user that plot creation is starting:
         print("Creating CAM diagnostic plots.")
+
+        #How to actually run plotting scripts:
+        #for plot_script in self.__plot_scripts:
+           #Add '()' to script name:
+        #   plot_func = plot_script+'()'
+        #   eval(plot_func)
+
 
 ###############
