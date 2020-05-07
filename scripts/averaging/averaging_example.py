f11269d4d9e410ebaf2e5f809f9e9d08e6eacb7e
Jesse Nusbaumer <nusbaume@ucar.edu>
2020-05-07 15:41:24 -0600

    Replace 'namelist' with 'config', and fix bugs in various scripts.

--- scripts/averaging/averaging_example.py
+++ scripts/averaging/averaging_example.py
@@ -53,10 +53,13 @@ def averaging_example(case_name, input_ts_loc, output_loc, var_list):
         else:
             cam_ts_data = xr.open_mfdataset(ts_files, decode_times=True, combine='by_coords')
 
-        #Average time dimension over time bands, if the bands exist:
+        #Average time dimension over time bounds, if bounds exist:
         if 'time_bnds' in cam_ts_data:
-             cam_ts_data['time'].values = cam_ts_data['time_bnds'].mean(dim='nbnd')
-             cam_ts_data = xr.decode_cf(cam_ts_data)
+            time = cam_ts_data['time']
+            time = xr.DataArray(cam_ts_data['time_bnds'].mean(dim='nbnd').values, dims=time.dims, attrs=time.attrs)
+            cam_ts_data['time'] = time
+            cam_ts_data.assign_coords({'time':time})
+            cam_ts_data = xr.decode_cf(cam_ts_data)
 
         #Group time series values by month, and average those months together:
         cam_climo_data = cam_ts_data.groupby('time.month').mean(dim='time')
