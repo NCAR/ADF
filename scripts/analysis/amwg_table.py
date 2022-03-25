156383e45a70ab2dacb8d31e3124dc3c85b1f403
justin-richling <richling@ucar.edu>
2022-03-25 13:05:46 -0600

    update amwg_table.py

--- scripts/analysis/amwg_table.py
+++ scripts/analysis/amwg_table.py
@@ -331,7 +331,7 @@ def _df_comp_table(write_html,output_location,case_names):
         output_html_file_comp = output_location / "amwg_table_comp.html"
     
     html = df_comp.to_html(index=False, border=1, justify='center', float_format='{:,.3g}'.format)  # should return string
-    preamble = f"""<html><head></head><body><h1>AMWG Case Comparison<h1>"""
+    preamble = f"""<html><head></head><body><h1>AMWG Case Comparison<h1><h2>Test Case: {case_names[0]}<br/>Control Case: {case_names[1]}</h2>"""
     ending = """</body></html>"""
     with open(output_html_file_comp, 'w') as hfil:
         hfil.write(preamble)
