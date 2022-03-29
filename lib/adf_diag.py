76b1f8d753943f03adfa28786798fd5f5f50694c
justin-richling <richling@ucar.edu>
2022-03-29 15:46:10 -0600

    attempt - 2

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -1022,7 +1022,7 @@ class AdfDiag(AdfObs):
 
                 #Move colored table html file to new directory
                 for color_table in color_table_html_file:
-                    shutil.move(color_table, table_pages_dir / color_table.name)
+                    shutil.copy2(color_table, table_pages_dir / color_table.name)
 
                 #Move all case table html files to new directory:
                 for table_html in table_html_files:
