23982cc446360639ee2db381049293168546e221
justin-richling <richling@ucar.edu>
2022-04-01 15:16:04 -0600

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -988,7 +988,8 @@ class AdfDiag(AdfObs):
             mean_rndr = mean_tmpl.render(title=main_title,
                             case1=case_name,
                             case2=data_name,
-                            mydata=mean_html_info)
+                            mydata=mean_html_info,
+                            plot_types=plot_type_html)
 
             #Write mean diagnostic plots HTML file:
             outputfile = img_pages_dir / "mean_diag.html"
@@ -1081,7 +1082,8 @@ class AdfDiag(AdfObs):
                 mean_rndr = mean_tmpl.render(title=main_title,
                                 case1=case_name,
                                 case2=data_name,
-                                amwg_tables=amwg_tables)
+                                amwg_tables=amwg_tables,
+                                plot_types=plot_type_html)
 
                 #Write mean diagnostic tables HTML file:
                 outputfile = table_pages_dir / "mean_table.html"
