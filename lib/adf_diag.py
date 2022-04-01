2ff0b131aa86d42565bf99bca1d39b408fd9caa4
justin-richling <richling@ucar.edu>
2022-04-01 15:17:05 -0600

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -886,6 +886,8 @@ class AdfDiag(AdfObs):
 
         #Set preferred order of plot types:
         plot_type_order = ["LatLon", "Zonal", "NHPolar", "SHPolar"]
+        plot_type_web = ["","","",""]
+        plot_type_html = dict(zip(plot_type_order, plot_type_web))
 
         #Also add pressure level Lat-Lon plots, if applicable:
         pres_levs = self.get_basic_info("plot_press_levels")
@@ -1101,11 +1103,10 @@ class AdfDiag(AdfObs):
             #plot_type_order_web = plot_type_order
 
             #plot_type_html = OrderedDict()
-            plot_type_web_name = ["Tables"]+plot_type_order
-            mean_table = "https://project.cgd.ucar.edu/projects/ADF/web_update/f.e21.FWscHIST.ne30_L48_BL10_cam6_3_041_kzz3_zmtop75.hf.001_vs_f.e21.FWscHIST.ne30_L48_BL10_cam6_3_041_control.hf.001_1982_1990/website/html_table/mean_table.html"
-            mean_table = str(outputfile)
-            plot_type_web = ["","","",""]
-            plot_type_html = dict(zip(plot_type_order, plot_type_web))
+            #plot_type_web_name = ["Tables"]+plot_type_order
+            #mean_table = "https://project.cgd.ucar.edu/projects/ADF/web_update/f.e21.FWscHIST.ne30_L48_BL10_cam6_3_041_kzz3_zmtop75.hf.001_vs_f.e21.FWscHIST.ne30_L48_BL10_cam6_3_041_control.hf.001_1982_1990/website/html_table/mean_table.html"
+            #mean_table = str(outputfile)
+            
          
 
             #Construct index.html
