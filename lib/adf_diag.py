d356dc4b26930eb088ded90c74aca9a7ccc20355
justin-richling <richling@ucar.edu>
2022-04-01 15:10:22 -0600

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -1102,8 +1102,8 @@ class AdfDiag(AdfObs):
             plot_type_web_name = ["Tables"]+plot_type_order
             mean_table = "https://project.cgd.ucar.edu/projects/ADF/web_update/f.e21.FWscHIST.ne30_L48_BL10_cam6_3_041_kzz3_zmtop75.hf.001_vs_f.e21.FWscHIST.ne30_L48_BL10_cam6_3_041_control.hf.001_1982_1990/website/html_table/mean_table.html"
             mean_table = str(outputfile)
-            plot_type_web = [mean_table,"","","",""]
-            plot_type_html = dict(zip(plot_type_web_name, plot_type_web))
+            plot_type_web = ["","","",""]
+            plot_type_html = dict(zip(plot_type_order, plot_type_web))
          
 
             #Construct index.html
