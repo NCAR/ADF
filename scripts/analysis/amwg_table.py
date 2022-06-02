a47d3309f6fffeeb5adf8aed03cfdd500148680b
Cecile Hannay <hannay@ucar.edu>
2022-06-02 10:05:23 -0600

    Update scripts/analysis/amwg_table.py
    Co-authored-by: Jesse Nusbaumer <nusbaume@ucar.edu>

--- scripts/analysis/amwg_table.py
+++ scripts/analysis/amwg_table.py
@@ -196,7 +196,7 @@ def amwg_table(adf):
 
             #Check if variable has a vertical coordinate:
             if 'lev' in data.coords or 'ilev' in data.coords:
-                print("\t   Variable '{}' has a vertical dimension, ".format(var)+\
+                print(f"\t   Variable '{var}' has a vertical dimension, "+\
                       "which is currently not supported for the AMWG Table. Skipping...")
                 #Skip this variable and move to the next variable in var_list:
                 continue
