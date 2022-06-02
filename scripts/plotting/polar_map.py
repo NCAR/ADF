fe2fde8dc40af6644be286d7baf67b2c3fb94a0e
Cecile Hannay <hannay@ucar.edu>
2022-06-02 10:43:52 -0600

    Update scripts/plotting/polar_map.py
    Co-authored-by: Jesse Nusbaumer <nusbaume@ucar.edu>

--- scripts/plotting/polar_map.py
+++ scripts/plotting/polar_map.py
@@ -264,7 +264,7 @@ def polar_map(adfobj):
                     #End if (dimensions check)
 
                 else: #odata dimensions check
-                     print("\t   INFO: Skipping polar map for {} as it doesn't have only lat/lon dims.".format(var))
+                     print(f"\t   INFO: Skipping polar map for {var} as it doesn't have only lat/lon dims.")
 
                 #End if (dimensions check)
             #End for (case loop)
