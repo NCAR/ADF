e519a3b9cb05b9d847579566e14317cf0b919d86
Brian Medeiros <brianpm@ucar.edu>
2022-09-07 16:28:18 -0600

    regional maps script

--- scripts/plotting/regional_map_multicase.py
+++ scripts/plotting/regional_map_multicase.py
@@ -112,64 +112,76 @@ def regional_map_multicase(adfobj):
     # Determine the list of variables to use (defaults to ADF's diag_var_list)
     #
     var_list = regional_opts.get("region_variables", adfobj.diag_var_list)
-
-    # < around here is where we add the plot to the website and delete the old version if redo >
-
     #
     # LOOP OVER VARIABLES
     #
     for v in var_list:
         # the reference case
         data_to_plot = {}
-        data_to_plot[data_name] = _retrieve(
+        refcasetmp = _retrieve(
             regional_opts, v, data_name, data_loc
         )  # get the baseline field
+        data_to_plot[data_name] = refcasetmp.mean(dim="time")
         # each of the other cases
         for casenumber, case in enumerate(case_names):
-            data_to_plot[case] = _retrieve(regional_opts, v, case, case_loc[casenumber])
+            casetmp = _retrieve(regional_opts, v, case, case_loc[casenumber])
+            data_to_plot[case] = casetmp.mean(dim="time")
         plot = regional_map_multicase_plot(
             adfobj, data_to_plot, opt=res.get(v)
         )  # res will be None if v not in the defaults
+        plot.suptitle(f"{v}, {sampling_str} Mean")
         # save the output
         plot_name = _construct_outfile_name(
             plot_loc, plot_type, v, sampling_str
         )  # this has to be consistent with web site generator
 
-        adfobj.add_website_data(plot_name, "Regional Maps", None, multi_case=True)
+        try: 
+            adfobj.add_website_data(plot_name, "Regional Maps", None, multi_case=True)
+        except:
+            print("Something is going wrong with adding to website")
+        plot.savefig(plot_name, bbox_inches='tight')
 
-        plot.savefig(plot_name)
 
+#
+# Helper functions
+# - _retrieve
+# - _get_month_number
+# - _get_sampling_string
+# - regional_map_multicase_plot
+# - _get_plot_location
+# - _construct_outfile_name
+# - autoscale_title
+# - simple_case_shortener
 
 def _retrieve(ropt, variable, casename, location, return_dataset=False):
-    """Custom function that retrieves a variable. Returns the variable as a DataArray.
+    """Custom function that retrieves a variable. 
+       Returns the variable as a DataArray, unless keword return_dataset=True.
 
     Applies the regional and time interval selections based in values in ropt.
 
-    ropt -> regional options specified via YAML
+    ropt -> dict, regional options specified via YAML
 
-    variable -> the variable to get from files
+    variable -> str, the variable to get from files
 
-    casename -> case name string for constructing file search
+    casename -> str, case name for constructing file search
 
-    location -> path to case
+    location -> Path, path to case
 
     kwarg:
-    return_dataset -> if true, return the dataset object, otherwise return the DataArray
+    return_dataset -> bool, if true, return the dataset object, otherwise return the DataArray
                       with `variable`
     """
     # note: this function assumes monthly data
-
-    fils = sorted(Path(location).glob(f"{casename}*_{variable}_*.nc"))
+    fils = sorted(Path(location).glob(f"{casename}*.{variable}.*.nc"))
     if len(fils) == 0:
         raise ValueError(f"something went wrong for variable: {variable}")
     elif len(fils) > 1:
         ds = xr.open_mfdataset(fils)  # allows climo files split into pieces
     else:
         ds = xr.open_dataset(fils[0])
-    if return_dataset:
-        da = ds
-    else:
-        da = ds[variable]
+    
+    # resampling is easier with DataArray
+    da = ds[variable]
 
     # Apply regional selection logic
     # -- assume lat/lon coords
@@ -184,10 +196,8 @@ def _retrieve(ropt, variable, casename, location, return_dataset=False):
             if ("region_start_year" in ropt) and ("region_end_year" in ropt):
                 da = da.loc[
                     {
-                        "time": da.time[
-                            (da.time.dt.year >= ropt["region_start_year"])
+                        "time": (da.time.dt.year >= ropt["region_start_year"])
                             & (da.time.dt.year <= ropt["region_end_year"])
-                        ]
                     }
                 ]
         elif ropt["region_time_option"] == "zeroanchor":
@@ -198,15 +208,12 @@ def _retrieve(ropt, variable, casename, location, return_dataset=False):
             min_year = da.time.dt.year.min()
             da = da.loc[
                 {
-                    "time": da.time[
-                        (da.time.dt.year >= min_year + ropt["region_year_offset"])
-                        & (
-                            da.time.dt.year
+                    "time": (da.time.dt.year >= min_year + ropt["region_year_offset"])
+                        & (da.time.dt.year
                             <= min_year
                             + ropt["region_year_offset"]
                             + ropt["region_nyear"]
                         )
-                    ]
                 }
             ]
         else:
@@ -214,50 +221,68 @@ def _retrieve(ropt, variable, casename, location, return_dataset=False):
 
     # -- select month, season, or annual average (precedence in that order)
     if "region_month" in ropt:
+        print("-->FOUND region_month in ropt")
         if ropt["region_month"] is not None:
             month_number = _get_month_number(ropt["region_month"])
+            print(f"--> Good, month_number = {month_number}")
             if month_number is not None:
-                da = da.loc[{"time": da.time[da.time.dt.month == month_number]}]
+                da = da.loc[{"time": da.time.dt.month == month_number}]
         else:
             month_number = None
     else:
         month_number = None
 
+    print(f"Okay second chance -> month_number = {month_number}")
     if (month_number is None) and ("region_season" in ropt):
         month_length = da.time.dt.days_in_month
         da = (da * month_length).resample(time="QS-DEC").sum(
             dim="time"
         ) / month_length.resample(time="QS-DEC").sum(dim="time")
-    else:
+    elif (month_number is None) and ("region_season" not in ropt):
         print("Regional map is defaulting to annual mean.")
         month_length = da.time.dt.days_in_month
         da = (da * month_length).groupby("time.year").sum(
             dim="time"
         ) / month_length.groupby("time.year").sum(dim="time")
-    return da
+        da = da.rename({"year":"time"})
+    # else assume month_number is not none
+    
+    if return_dataset:
+        return xr.Dataset(da)
+    else:
+        return da
 
 
 def _get_month_number(m):
+    """
+    Utility to get the number (1-12) from a string form.
+    
+    Input, `m`, can be string or integer. 
+    If it is a string, can be 3-letter abbreviation or full month name.
+    If integer, needs to be 1-12.
+    Emits a useful string if it looks like a season abbreviation.
+    """
     from time import strptime
-
     if isinstance(m, str):
         if len(m) == 3:
             if m in ["DJF", "MAM", "JJA", "SON"]:
                 print(f"Error: it looks like a season was supplied as a month: {m}")
-                return None  # fails if season was specified accidentally
-            n = strptime(m, "%b").tm_mon  # guessing abbreviation (may is ok)
+                n = None  # fails if season was specified accidentally
+            else:
+                n = strptime(m, "%b").tm_mon  # asumes 3-letter abbreviation (e.g., jan feb mar apr may ...)
         elif len(m) > 3:
-            n = strptime(m, "%B").tm_mon  # guessing full name
+            n = strptime(m, "%B").tm_mon  # guessing full name (january, february, ...)
         elif len(m) == 1:
             n = strptime(m, "%m").tm_mon  # guessing supplied the number
         else:
             print(f"ERROR: confused about what month is meant by {m}")
-            return None
+            n = None
     elif isinstance(m, int):
         return m
     else:
         print(f"Error: month supplied is neither string nor integer: {m}, {type(m)}")
         return None
+    return n
 
 
 def _get_sampling_string(ropt):
@@ -278,7 +303,11 @@ def _get_sampling_string(ropt):
     return result
 
 
+
 def regional_map_multicase_plot(adf, datadict, opt=None):
+    """
+    Make a figure with up to 10 cases in a row of maps for specified region.
+    """
     ncols = len(datadict)
     fig, ax = plt.subplots(
         figsize=(4 * ncols, 3),
@@ -290,16 +319,23 @@ def regional_map_multicase_plot(adf, datadict, opt=None):
     )
     [a.coastlines() for a in ax]
     cntrplts = []
+    titl = []
     if opt is not None:
         tiString = opt.pop("tiString", None)
         tiFontSize = opt.pop("tiFontSize", 8)
+    else:
+        opt = dict()  # make an empty dict if one is not given
+
+    # an attempt to set titles to case names ... short names would be better:
+    short_names = simple_case_shortener(datadict.keys())
 
     for i, xreg in enumerate(datadict):
-        lons, lats = np.meshgrid(xreg.lon, xreg.lat)
+        assert len(datadict[xreg].dims) == 2, f"ERROR: the array must be 2D... has dims {datadict[xreg].dims}"
+        lons, lats = np.meshgrid(datadict[xreg].lon, datadict[xreg].lat)
         #
         # to do: check opt for all the things: cmap, contour levels, etc.
         #
-        popts = prep_contour_plot(xreg, xreg, xreg - xreg, **opt)
+        popts = prep_contour_plot(datadict[xreg], datadict[xreg], datadict[xreg] - datadict[xreg], **opt)
         popts2 = {
             **popts["contourf_opt"],
             "cmap": popts["cmap1"],
@@ -308,13 +344,22 @@ def regional_map_multicase_plot(adf, datadict, opt=None):
         }
         cb_opt = popts.get("colorbar_opt")
         cntrplts.append(
-            ax[i].contourf(lons, lats, xreg, transform=ccrs.PlateCarree(), **popts2)
+            ax[i].contourf(lons, lats, datadict[xreg], transform=ccrs.PlateCarree(), **popts2)
         )  # can use ax[i] b/c it is only 1-d array of axes
+        titl.append(ax[i].set_title(short_names[i],loc='left'))
+        autoscale_title(titl[i], ax[i], fig)
+        gl = ax[i].gridlines(draw_labels=True, linewidth=0)
+        gl.top_labels = False
+        gl.right_labels = False
+        if ax[i].get_subplotspec().is_first_col():
+            gl.left_labels = True
+        else:
+            gl.left_labels = False
     cb = fig.colorbar(
         mpl.cm.ScalarMappable(norm=popts2["norm"], cmap=popts2["cmap"]),
         ax=ax,
         orientation="horizontal",
-        aspect=50,
+        aspect=60,
         **cb_opt,
     )
     return fig
@@ -361,3 +406,33 @@ def _construct_outfile_name(ploc, ptype, *args):
     """
     cat = "_".join(args)
     return ploc / f"{cat}_MultiCaseRegion_Mean.{ptype}"
+
+
+def autoscale_title(tiobj, ax, fig):
+    """Utility function that tries to shrink the title to fit on top of the Axes."""
+    renderer = fig.canvas.get_renderer()
+    fsz = tiobj.get_fontsize()
+    while (fsz>6) and (tiobj.get_window_extent(renderer=renderer).width > ax.bbox.width):
+        fsz -= 1
+        tiobj.set_fontsize(fsz)
+
+
+def simple_case_shortener(case_names):
+    """Reduce case names by removing a common prefix.
+    Short case names would be better!
+    """
+    szs = [len(c) for c in case_names]
+    minsize = np.min(szs)
+    for i in range(minsize):
+        chkchar = [c[i] for c in case_names]
+        if all(ele == chkchar[0] for ele in chkchar):
+            continue
+        else:
+            result = [ele[i:] for ele in case_names]
+            break
+    else:
+        result = None
+    if result is None:
+        return case_names
+    else:
+        return result
