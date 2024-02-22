a4d5600b0412c141d7301e5e6d8ba140ce8393e0
justin-richling <richling@ucar.edu>
2024-02-22 15:38:53 -0700

    Update adf_diag.py

--- lib/adf_diag.py
+++ lib/adf_diag.py
@@ -631,11 +631,11 @@ class AdfDiag(AdfWeb):
             # End variable loop
             print("here?")
             print(ts_outfil_str)
-            """
+            
             # Now run the "ncrcat" subprocesses in parallel:
             with mp.Pool(processes=self.num_procs) as mpool:
                 _ = mpool.map(call_ncrcat, list_of_commands)
-            """
+            
             #call_ncrcat(list_of_commands)
             #list_of_commands =["ncrcat", "-O", "-4", "-h", "--no_cll_mth", "-v", "SNOWDP", "/glade/campaign/cesm/development/cross-wg/diagnostic_framework/CESM_output_for_testing/ctsm51d159_f45_GSWP3_bgccrop_1850pAD/lnd/hist/ctsm51d159_f45_GSWP3_bgccrop_1850pAD.clm2.h0.0001-01.nc", "/glade/campaign/cesm/development/cross-wg/diagnostic_framework/CESM_output_for_testing/ctsm51d159_f45_GSWP3_bgccrop_1850pAD/lnd/hist/ctsm51d159_f45_GSWP3_bgccrop_1850pAD.clm2.h0.0001-02.nc", "-o", ts_outfil_str]
             
@@ -649,7 +649,7 @@ class AdfDiag(AdfWeb):
             #print(list_of_commands)
             #list_of_commands.append("-o")
             #list_of_commands.append(ts_outfil_str)
-            subprocess.run(list_of_commands, shell=False)
+            #subprocess.run(list_of_commands, shell=False)
 
             if vars_to_derive:
                 self.derive_variables(
