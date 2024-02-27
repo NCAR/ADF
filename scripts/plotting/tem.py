179338845c8ad4af49337ae2d51a50b9bfca7ce0
justin-richling <richling@ucar.edu>
2024-02-27 12:02:31 -0700

    Update tem website object

--- scripts/plotting/tem.py
+++ scripts/plotting/tem.py
@@ -152,7 +152,7 @@ def tem(adf):
         if (not redo_plot) and plot_name.is_file():
             #Add already-existing plot to website (if enabled):
             adf.debug_log(f"'{plot_name}' exists and clobber is false.")
-            adf.add_website_data(plot_name, "TEM", None, season=s)
+            adf.add_website_data(plot_name, "TEM", None, season=s, multi_case=True)
 
             #Continue to next iteration:
             continue
@@ -194,7 +194,7 @@ def tem(adf):
         fig.savefig(plot_name, bbox_inches='tight', dpi=300)
 
         #Add plot to website (if enabled):
-        adf.add_website_data(plot_name, "TEM", case_name, season=s)
+        adf.add_website_data(plot_name, "TEM", None, season=s, multi_case=True)
 
     print("  ...TEM plots have been generated successfully.")
 
