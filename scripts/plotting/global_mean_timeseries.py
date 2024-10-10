224158d0a18d3d4b638cbfaa8617512fbd0dd5c4
Brian Medeiros <brianpm@ucar.edu>
2024-10-10 14:24:10 -0600

    refactor to modular functions and classes

--- scripts/plotting/global_mean_timeseries.py
+++ scripts/plotting/global_mean_timeseries.py
@@ -1,15 +1,19 @@
+"""Use time series files to produce global mean time series plots for ADF web site.
+
+Includes a minimal Class for bringing CESM2 LENS data 
+from I. Simpson's directory (to be generalized).
+
+"""
+
 from pathlib import Path
-import numpy as np
-import xarray as xr
-
-import matplotlib.pyplot as plt
-
-import plotting_functions as pf
-
 import warnings  # use to warn user about missing files.
+import xarray as xr
+import matplotlib.pyplot as plt
+import plotting_functions as pf
 
 
 def my_formatwarning(msg, *args, **kwargs):
+    """custom warning"""
     # ignore everything except the message
     return str(msg) + "\n"
 
@@ -17,24 +21,87 @@ def my_formatwarning(msg, *args, **kwargs):
 warnings.formatwarning = my_formatwarning
 
 
-INCLUDE_LENS2 = True
-
-
 def global_mean_timeseries(adfobj):
     """
     load time series file, calculate global mean, annual mean
     for each case
-    Make a combined plot
-    Optionally show the CESM2 LENS result.
+    Make a combined plot, save it, add it to website.
+    Include the CESM2 LENS result if it can be found.
     """
-    var_list = adfobj.diag_var_list
 
-    # Special ADF variable which contains the output paths for
-    # all generated plots and tables:
-    basic_info_dict = adfobj.read_config_var("diag_basic_info")
-    plot_locations = adfobj.plot_location
+    plot_loc = get_plot_loc(adfobj)
 
-    # ADF variable which contains the output path for plots and tables:
+    plot_type = adfobj.read_config_var("diag_basic_info").get("plot_type", "png")
+
+    for field in adfobj.diag_var_list:
+        # reference time series (DataArray)
+        ref_ts_da = adfobj.data.load_reference_timeseries_da(field)
+        # check if this is a "2-d" varaible:
+        has_lat_ref, has_lev_ref = pf.zm_validate_dims(ref_ts_da)
+        if has_lev_ref:
+            print(
+                f"Variable named {field} has a lev dimension, which does not work with this script."
+            )
+            continue
+
+        # reference time series global average
+        ref_ts_da_ga = pf.spatial_average(ref_ts_da, weights=None, spatial_dims=None)
+        # annually averaged
+        ref_ts_da = pf.annual_mean(ref_ts_da_ga, whole_years=True, time_name="time")
+
+        ## SPECIAL SECTION -- CESM2 LENS DATA:
+        lens2_data = Lens2Data(
+            field
+        )  # Provides access to LENS2 dataset when available (class defined below)
+
+        # Loop over model cases:
+        case_ts = {}  # dictionary of annual mean, global mean time series
+        for case_name in adfobj.data.case_names:
+            c_ts_da = adfobj.data.load_timeseries_da(case_name, field)
+            c_ts_da_ga = pf.spatial_average(c_ts_da)
+            case_ts[case_name] = pf.annual_mean(c_ts_da_ga)
+        # now have to plot the timeseries
+        fig, ax = make_plot(
+            ref_ts_da, case_ts, lens2_data, label=adfobj.data.ref_nickname
+        )
+        plot_name = plot_loc / f"{field}_GlobalMean_ANN_TimeSeries_Mean.{plot_type}"
+
+        conditional_save(adfobj, plot_name, fig)
+
+        adfobj.add_website_data(
+            plot_name,
+            f"{field}_GlobalMean",
+            None,
+            season="ANN",
+            multi_case=True,
+            plot_type="TimeSeries",
+        )
+
+
+def conditional_save(adfobj, plot_name, fig):
+    """Determines whether to save figure"""
+    # double check this
+    if adfobj.get_basic_info("redo_plot") and plot_name.is_file():
+        # Case 1: Delete old plot, save new plot
+        plot_name.unlink()
+        fig.savefig(plot_name)
+    elif (adfobj.get_basic_info("redo_plot") and not plot_name.is_file()) or (
+        not adfobj.get_basic_info("redo_plot") and not plot_name.is_file()
+    ):
+        # Save new plot
+        fig.savefig(plot_name)
+    elif not adfobj.get_basic_info("redo_plot") and plot_name.is_file():
+        # Case 2: Keep old plot, do not save new plot
+        print("plot file detected, redo is false, so keep existing file.")
+    else:
+        warnings.warn(f"Conditional save found unknown condition. File will not be written: {plot_name}")
+    plt.close(fig)
+
+
+def get_plot_loc(adfobj):
+    """Return the path for plot files.
+    Contains side-effect: will make the directory and parents if needed.
+    """
     plot_location = adfobj.plot_location
     if not plot_location:
         plot_location = adfobj.get_basic_info("cam_diag_plot_loc")
@@ -54,108 +121,50 @@ def global_mean_timeseries(adfobj):
             plot_loc = Path(plot_location[0])
     else:
         plot_loc = Path(plot_location)
-    print(f"YOUR PLOT LOCATION: {plot_loc}")
+    print(f"Determined plot location: {plot_loc}")
+    return plot_loc
 
-    plot_type = basic_info_dict.get("plot_type", "png")
 
-    print(f"CHECK ON PLOT LOCATION: {plot_locations}")
+class Lens2Data:
+    """Access Isla's LENS2 data to get annual means."""
+    def __init__(self, field):
+        self.field = field
+        self.has_lens, self.lens2 = self._include_lens()
 
-    for field in var_list:
-        # reference time series
-        # files
-        # ref_ts_fil = adfobj.data.get_ref_timeseries_file(field)
-        # dataset
-        # ref_ts_ds = adfobj.data.load_timeseries_dataset(ref_ts_fil)
-
-        # DataArray
-        ref_ts_da = adfobj.data.load_reference_timeseries_da(field)
-        # check if this is a "2-d" varaible:
-        # has_lat_ref, has_lev_ref = pf.zm_validate_dims(ref_ts_ds[field])
-        has_lat_ref, has_lev_ref = pf.zm_validate_dims(ref_ts_da)
-        if has_lev_ref:
-            print(
-                f"Variable named {field} has a lev dimension, which does not work with this script."
-            )
-            continue
-
-        # dataarray (global average)
-        ref_ts_da_ga = pf.spatial_average(
-            ref_ts_da, weights=None, spatial_dims=None
+    def _include_lens(self):
+        lens2_fil = Path(
+            f"/glade/campaign/cgd/cas/islas/CESM_DATA/LENS2/global_means/annualmeans/{self.field}_am_LENS2_first50.nc"
         )
-        # dataarray (annually averaged)
-        ref_ts_da = pf.annual_mean(ref_ts_da_ga, whole_years=True, time_name="time")
-
-        ## SPECIAL SECTION -- CESM2 LENS DATA:
-        if INCLUDE_LENS2:
-            lens2_fil = Path(
-                f"/glade/campaign/cgd/cas/islas/CESM_DATA/LENS2/global_means/annualmeans/{field}_am_LENS2_first50.nc"
-            )
         if lens2_fil.is_file():
-            # print("Success: Found LENS2 file.")
             lens2 = xr.open_mfdataset(lens2_fil)
-            have_lens = True
+            has_lens = True
         else:
             warnings.warn("Failure: DID NOT FIND LENS2 FILE.")
-            have_lens = False
+            has_lens = False
+            lens2 = None
+        return has_lens, lens2
 
-        # Loop over model cases:
-        case_ts = {}  # dictionary of annual mean, global mean time series
-        # same steps as in ref
-        for case_idx, case_name in enumerate(adfobj.data.case_names):
-            # cfil = adfobj.data.get_timeseries_file(case_name, field)
-            # c_ts_ds = adfobj.data.load_timeseries_dataset(cfil)
-            c_ts_da = adfobj.data.load_timeseries_da(case_name, field)
-            c_ts_da_ga = pf.spatial_average(c_ts_da)
-            case_ts[case_name] = pf.annual_mean(c_ts_da_ga)
-        # now have to plot the timeseries
-        fig, ax = plt.subplots()
-        ax.plot(ref_ts_da.year, ref_ts_da, label=adfobj.data.ref_nickname)
-        for c, cdata in case_ts.items():
-            ax.plot(cdata.year, cdata, label=c)
-        if have_lens:
-            lensmin = lens2[field].min("M")
-            lensmax = lens2[field].max("M")
-            ax.fill_between(
-                lensmin.year, lensmin, lensmax, color="lightgray", alpha=0.5
-            )
-            ax.plot(
-                lens2.year,
-                lens2[field].mean("M"),
-                color="darkgray",
-                linewidth=2,
-                label="LENS2",
-            )
 
-        ax.set_title(field, loc="left")
-        ax.set_xlabel("YEAR")
-        fig.legend()
-        plot_name = plot_loc / f"{field}_GlobalMean_ANN_TimeSeries_Mean.{plot_type}"
-
-        # double check this
-        redo_plot = adfobj.get_basic_info("redo_plot")
-        file_exists = plot_name.is_file()
-        if redo_plot and file_exists:
-            # Case 1: Delete old plot, save new plot
-            plot_name.unlink()
-            fig.savefig(plot_name)
-            plt.close(fig)
-        elif not redo_plot and file_exists:
-            # Case 2: Keep old plot, do not save new plot
-            plt.close(fig)
-        elif redo_plot and not file_exists:
-            # Case 3: Save new plot
-            fig.savefig(plot_name)
-            plt.close(fig)
-        elif not redo_plot and not file_exists:
-            # Case 4: Save new plot
-            fig.savefig(plot_name)
-            plt.close(fig)
-
-        adfobj.add_website_data(
-            plot_name,
-            f"{field}_GlobalMean",
-            None,
-            season="ANN",
-            multi_case=True,
-            plot_type="TimeSeries",
+def make_plot(ref_ts_da, case_ts, lens2, label=None):
+    """plot yearly values of ref_ts_da"""
+    fig, ax = plt.subplots()
+    ax.plot(ref_ts_da.year, ref_ts_da, label=label)
+    for c, cdata in case_ts.items():
+        ax.plot(cdata.year, cdata, label=c)
+    if lens2.has_lens:
+        field = lens2.field
+        lensmin = lens2.lens2[field].min("M")  # note: "M" is the member dimension
+        lensmax = lens2.lens2[field].max("M")
+        ax.fill_between(lensmin.year, lensmin, lensmax, color="lightgray", alpha=0.5)
+        ax.plot(
+            lens2.lens2[field].year,
+            lens2.lens2[field].mean("M"),
+            color="darkgray",
+            linewidth=2,
+            label="LENS2",
         )
+
+    ax.set_title(field, loc="left")
+    ax.set_xlabel("YEAR")
+    fig.legend()
+    return fig, ax
