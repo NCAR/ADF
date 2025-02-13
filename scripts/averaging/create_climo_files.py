fc0e8ec43bb79af20f76ba97643e06a61961a07f
justin-richling <richling@ucar.edu>
2025-02-13 11:14:53 -0700

    Update create_climo_files.py
    Main point here is to add new cleaned up calls for the config yaml file args from adf_info.
    
    Also, add a check to see if there are multiple time series files for climo creation, and if so don't use multiprocessing

--- scripts/averaging/create_climo_files.py
+++ scripts/averaging/create_climo_files.py
@@ -71,10 +71,10 @@ def create_climo_files(adf, clobber=False, search=None):
 
     #CAM simulation variables (These quantities are always lists):
     case_names    = adf.get_cam_info("cam_case_name", required=True)
-    input_ts_locs = adf.get_cam_info("cam_ts_loc", required=True)
-    output_locs   = adf.get_cam_info("cam_climo_loc", required=True)
-    calc_climos   = adf.get_cam_info("calc_cam_climo")
+    output_locs = adf.climo_locs["test"]
     overwrite     = adf.get_cam_info("cam_overwrite_climo")
+    calc_climos = adf.calc_climos["test"]
+    input_ts_locs = adf.ts_locs["test"]
 
     #Extract simulation years:
     start_year = adf.climo_yrs["syears"]
@@ -82,20 +82,25 @@ def create_climo_files(adf, clobber=False, search=None):
 
     #If variables weren't provided in config file, then make them a list
     #containing only None-type entries:
-    if not calc_climos:
-        calc_climos = [None]*len(case_names)
     if not overwrite:
         overwrite = [None]*len(case_names)
-    #End if
+    else:
+        #Check if any time series files are pre-made
+        if len(overwrite) != len(case_names):
+            print("We have a problem, the number of overwrite does not match the number of cases!")
+            #adf.error thingy
+
+
 
     #Check if a baseline simulation is also being used:
     if not adf.get_basic_info("compare_obs"):
         #Extract CAM baseline variaables:
         baseline_name     = adf.get_baseline_info("cam_case_name", required=True)
-        input_ts_baseline = adf.get_baseline_info("cam_ts_loc", required=True)
-        output_bl_loc     = adf.get_baseline_info("cam_climo_loc", required=True)
-        calc_bl_climos    = adf.get_baseline_info("calc_cam_climo")
+        output_bl_loc = adf.climo_locs["baseline"]
+        #calc_bl_climos    = adf.get_baseline_info("calc_cam_climo")
         ovr_bl            = adf.get_baseline_info("cam_overwrite_climo")
+        calc_bl_climos = adf.calc_climos["baseline"]
+        input_ts_baseline = adf.ts_locs["baseline"]
 
         #Extract baseline years:
         bl_syr = adf.climo_yrs["syear_baseline"]
@@ -123,6 +128,9 @@ def create_climo_files(adf, clobber=False, search=None):
         #Check if climatology is being calculated.
         #If not then just continue on to the next case:
         if not calc_climos[case_idx]:
+            emsg = "\t    INFO: Configuration file indicates climo files have been pre-computed"
+            emsg += f" for case '{case_name}'.  Will rely on those files directly."
+            print(emsg)
             continue
 
         #Notify user of model case being processed:
@@ -149,16 +157,12 @@ def create_climo_files(adf, clobber=False, search=None):
             print(f"\t    {output_location} not found, making new directory")
             output_location.mkdir(parents=True)
 
-        # If we need to allow custom search, could put it into adf.data
-        # #Time series file search
-        # if search is None:
-        #     search = "{CASE}*{HIST_STR}*.{VARIABLE}.*nc"  # NOTE: maybe we should not care about the file extension part at all, but check file type later?
-
         #Check model year bounds:
         syr, eyr = check_averaging_interval(start_year[case_idx], end_year[case_idx])
 
         #Loop over CAM output variables:
         list_of_arguments = []
+        nums = []
         for var in var_list:
             # Notify user of new climo file:
             print(f"\t - climatology for {var}")
@@ -190,18 +194,21 @@ def create_climo_files(adf, clobber=False, search=None):
                 logmsg = f"climo file generation: The input location searched was: {input_location}. The glob pattern was {ts_files}."
                 #Write to debug log if enabled:
                 adf.debug_log(logmsg)
-                #  end_diag_script(errmsg) # Previously we would kill the run here.
                 continue
-
+            if len(ts_files) > 1:
+                process_variable(adf, ts_files, syr, eyr, output_file)
+            else:
+                nums.append("yup")
+                list_of_arguments.append((adf, ts_files, syr, eyr, output_file))
             list_of_arguments.append((adf, ts_files, syr, eyr, output_file))
 
 
         #End of var_list loop
         #--------------------
-
-        # Parallelize the computation using multiprocessing pool:
-        with mp.Pool(processes=number_of_cpu) as p:
-            result = p.starmap(process_variable, list_of_arguments)
+        if len(nums) > 0: 
+            # Parallelize the computation using multiprocessing pool:
+            with mp.Pool(processes=number_of_cpu) as p:
+                p.starmap(process_variable, list_of_arguments)
 
     #End of model case loop
     #----------------------
@@ -218,10 +225,24 @@ def process_variable(adf, ts_files, syr, eyr, output_file):
     Compute and save the climatology file.
     '''
     #Read in files via xarray (xr):
-    if len(ts_files) == 1:
-        cam_ts_data = xr.open_dataset(ts_files[0], decode_times=True)
-    else:
-        cam_ts_data = xr.open_mfdataset(ts_files, decode_times=True, combine='by_coords')
+    cam_ts_data = adf.data.load_dataset(ts_files)
+
+    # Exit function if data array is None
+    if not cam_ts_data:
+        return 0
+    #Create a dictionary of attributes
+    #Convert the list to a string (join with commas)
+    ts_files_str = [str(path) for path in ts_files]
+    ts_files_str = ', '.join(ts_files_str)
+    attrs_dict = {
+            "adf_user": adf.user,
+            "adf_climo_yrs": f"{syr}-{eyr}",
+            "adf_climo_generation_source": "Climo file was generated by xarray 'groupby' and averaged over time dimension",
+            "climatology_info": "'time' dimension is actually in months (1-12)",
+            "xarray_time_slice_values": f"Subset includes time values: {actual_time_values[0]} to {actual_time_values[-1]}",
+            "time_series_files": ts_files_str,
+        }
+
     #Average time dimension over time bounds, if bounds exist:
     if 'time_bnds' in cam_ts_data:
         time = cam_ts_data['time']
@@ -233,6 +254,15 @@ def process_variable(adf, ts_files, syr, eyr, output_file):
     #Extract data subset using provided year bounds:
     tslice = get_time_slice_by_year(cam_ts_data.time, int(syr), int(eyr))
     cam_ts_data = cam_ts_data.isel(time=tslice)
+    #Retrieve the actual time values from the slice
+    #NOTE: This is in place in case of premade climo files to make sure it is grabbing the correct time slice
+    actual_time_values = cam_ts_data.time.values
+    attrs_dict["xarray_time_slice_values"] = actual_time_values
+    msg = f"Checking to make sure dataarray is being sliced in the time dimension correctly: {actual_time_values}"
+
+    adf.debug_log(f"create_climo_files: {msg}")
+    #Set a global attribute with the actual time values
+
     #Group time series values by month, and average those months together:
     cam_climo_data = cam_ts_data.groupby('time.month').mean(dim='time')
     #Rename "months" to "time":
@@ -242,15 +272,7 @@ def process_variable(adf, ts_files, syr, eyr, output_file):
     enc_c  = {xname: {'_FillValue': None} for xname in cam_climo_data.coords}
     enc    = {**enc_c, **enc_dv}
 
-    # Create a dictionary of attributes
-    # Convert the list to a string (join with commas)
-    ts_files_str = [str(path) for path in ts_files]
-    ts_files_str = ', '.join(ts_files_str)
-    attrs_dict = {
-        "adf_user": adf.user,
-        "climo_yrs": f"{syr}-{eyr}",
-        "time_series_files": ts_files_str,
-    }
+    # Assign attributes to saved climo file
     cam_climo_data = cam_climo_data.assign_attrs(attrs_dict)
 
     #Output variable climatology to NetCDF-4 file:
