a9e9c70a6e80454d7f48d197121a80c8d7db7482
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-02-18 12:53:59 -0700

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -1649,11 +1649,12 @@ def lev_to_plev(data, ps, hyam, hybm, P0=100000., new_levels=None,
                                                                     hybm,
                                                                     p0=P0
                                                                    )
-
+    print("asf\n")
     # data_interp may contain a dask array, which can cause
     # trouble downstream with numpy functions, so call compute() here.
     if hasattr(data_interp, "compute"):
         data_interp = data_interp.compute()
+    print("asf 2\n")
 
     #Rename vertical dimension back to "lev" in order to work with
     #the ADF plotting functions:
