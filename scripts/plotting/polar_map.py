42a16fbcac15ea8b26a8c7c9ca27ccba392cb88c
Brian Medeiros <skymath@gmail.com>
2025-03-13 16:52:15 -0600

    check for levels in the variable earlier, improve logic for pressure level plots

--- scripts/plotting/polar_map.py
+++ scripts/plotting/polar_map.py
@@ -117,29 +117,36 @@ def polar_map(adfobj):
         
         for case_idx, case_name in enumerate(case_names):
             plot_loc = Path(plot_locations[case_idx])
-            
+
+            tmp_ds = adfobj.data.load_regrid_dataset(case_name, var)
+            if tmp_ds is None:
+                continue
+
+            has_lev = "lev" in tmp_ds.dims
+
             for s in seasons:
                 for hemi_type in ["NHPolar", "SHPolar"]:
                     if pres_levs:
-                        for pres in pres_levs:
-                            plot_name = plot_loc / f"{var}_{pres}hpa_{s}_{hemi_type}_Mean.{plot_type}"
-                            info = {
-                                'path': plot_name,
-                                'var': f"{var}_{pres}hpa",
-                                'case': case_name,
-                                'case_idx': case_idx,
-                                'season': s,
-                                'type': hemi_type,
-                                'pressure': pres,
-                                'exists': plot_name.is_file()
-                            }
-                            plot_info.append(info)
-                            if not (redo_plot or not info['exists']):
-                                adfobj.add_website_data(info['path'], info['var'],
-                                                      info['case'], category=web_category,
-                                                      season=s, plot_type=hemi_type)
-                            else:
-                                all_plots_exist = False
+                        if has_lev:
+                            for pres in pres_levs:
+                                plot_name = plot_loc / f"{var}_{pres}hpa_{s}_{hemi_type}_Mean.{plot_type}"
+                                info = {
+                                    'path': plot_name,
+                                    'var': f"{var}_{pres}hpa",
+                                    'case': case_name,
+                                    'case_idx': case_idx,
+                                    'season': s,
+                                    'type': hemi_type,
+                                    'pressure': pres,
+                                    'exists': plot_name.is_file()
+                                }
+                                plot_info.append(info)
+                                if not (redo_plot or not info['exists']):
+                                    adfobj.add_website_data(info['path'], info['var'],
+                                                        info['case'], category=web_category,
+                                                        season=s, plot_type=hemi_type)
+                                else:
+                                    all_plots_exist = False
                     else:
                         plot_name = plot_loc / f"{var}_{s}_{hemi_type}_Mean.{plot_type}"
                         info = {
