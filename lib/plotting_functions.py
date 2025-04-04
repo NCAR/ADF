ca6bf7907c0e67f7f101deb6b0014ba5b01a60ed
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-04-04 10:52:27 -0600

    Update plotting_functions.py

--- lib/plotting_functions.py
+++ lib/plotting_functions.py
@@ -792,7 +792,6 @@ def make_polar_plot(wks, case_nickname, base_nickname,
         pct = ux.UxDataArray(pct_0,uxgrid=pct_grid)
     else:
         pct = pct_0
-    print("What type is this!?!?!?!?",type(pct),"\n")
 
     if (hemisphere.upper() == "NH") or (hemisphere == "Arctic"):
         proj = ccrs.NorthPolarStereo()
